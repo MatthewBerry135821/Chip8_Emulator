5a9f3e79b24fac259c3499f61e7b2a1644e02daa
MatthewBerry135821 <mb135821@ohio.edu>
2023-01-24 20:10:15 -0500

    added class files

---
+++ cpu.h
@@ -0,0 +1,6 @@
+#ifndef CPU_H
+#define CPU_H
+class Cpu{
+
+};
+#endif
---
+++ system.h
@@ -0,0 +1,10 @@
+#include "screen.h"
+#include "memory.h"
+#include "cpu.h"
+#ifndef SYSTEM_H
+#define SYSTEM_H
+class System{
+    Screen screen;
+    
+};
+#endif
