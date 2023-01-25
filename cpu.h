81368fdbc82b44cc2e1312910305ae1cf9183b8c
MatthewBerry135821 <mb135821@ohio.edu>
2023-01-24 20:58:34 -0500

    Initial setup of Cpu class

--- cpu.h
+++ cpu.h
@@ -1,6 +1,19 @@
+#include "system.h"
 #ifndef CPU_H
 #define CPU_H
 class Cpu{
-
+    public:
+        Cpu(System *s = nullptr){system = s;};
+        void processNextInstruction();
+    private:
+        System *system;
+        uint8_t stack[64];
+        uint8_t generalPurposeRegisters[0xF];
+        uint16_t registerIndex;
+        bool registerVF;
+        uint8_t delayTimer;
+        uint8_t soundTimer;
+        uint8_t stackPointer;
+        uint16_t programCounter;
 };
 #endif
