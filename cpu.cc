1d79e353a09c7d2d480beff795bbc945ea627af0
MonadoBarrage <chenkatti@tutanota.com>
2023-01-27 22:17:57 -0500

    Edits in CPU.cc

--- cpu.cc
+++ cpu.cc
@@ -10,10 +10,50 @@ void Cpu::processNextInstruction(){
     uint16_t opCode;
     opCode = system.memory.loadIntruction(programCounter);
     switch(opCode & 0xF000){//switches the first nibble in the opCode. This allows instructions with parameters to implemented easier 
-        case 0x100:
+        case 0x1000:
+            
         break;
-        case 0x200:
+        case 0x2000:
+
         break;
+
+        case 0x5000:
+        //skip mean go to next thing on list
+             
+            if(generalPurposeRegisters[(opCode & 0x0F00) >> 8] == generalPurposeRegisters[(opCode & 0x0F0) >> 4]){
+                programCounter+=2;
+            }
+        break;
+
+        case 0x8000:
+            switch(opCode & 0x000F){
+                case 0x0000:
+                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x00F0) >> 4];
+                break;
+
+                case 0x0001:
+                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] | generalPurposeRegisters[(opCode & 0x00F0) >> 4];
+                    
+                break;
+                case 0x0002:
+                break;
+                case 0x0003:
+                break;
+                case 0x0004:
+                break;
+                case 0x0006:
+                break;
+                case 0x0007:
+                break;
+                case 0x000E:
+                break;
+                default:
+                break;
+
+
+            }
+        break;
+        
     }
     programCounter += 2;//programCounter stores location of the currently executing op code so it should be updated after it has been processed not when loaded
 }
