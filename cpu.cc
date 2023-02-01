2035c00bb6bf888615561f2382411700f25a3df5
Xofo66 <naryablanco@gmail.com>
2023-02-01 15:34:52 -0500

    Added_Opcodes

--- cpu.cc
+++ cpu.cc
@@ -11,7 +11,7 @@ void Cpu::processNextInstruction(){
     opCode = system.memory.loadIntruction(programCounter);
     switch(opCode & 0xF000){//switches the first nibble in the opCode. This allows instructions with parameters to implemented easier 
         case 0x1000:
-            programCounter = (opCode & 0x0FFF);
+            
         break;
         case 0x2000:
 
@@ -27,49 +27,30 @@ void Cpu::processNextInstruction(){
 
         case 0x8000:
             switch(opCode & 0x000F){
-                case 0x0000: //8xy0 - LD Vx, Vy
+                case 0x0000:
                     generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x00F0) >> 4];
                 break;
-                
-                case 0x0001: //8xy1 - OR Vx, Vy
-                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] | generalPurposeRegisters[(opCode & 0x00F0) >> 4];    
+
+                case 0x0001:
+                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] | generalPurposeRegisters[(opCode & 0x00F0) >> 4];
+                    
                 break;
-                
-                case 0x0002: //8xy2 - AND Vx, Vy
-                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] & generalPurposeRegisters[(opCode & 0x00F0) >> 4];
+                case 0x0002:
                 break;
-                
-                case 0x0003: //8xy3 - XOR Vx, Vy
-                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] ^ generalPurposeRegisters[(opCode & 0x00F0) >> 4];
+                case 0x0003:
                 break;
-                
-                case 0x0004: //8xy4 - ADD Vx, Vy
-                    registerVF = (generalPurposeRegisters[(opCode & 0x0F00) >> 8] + generalPurposeRegisters[(opCode & 0x00F0) >> 4]) > 0xFF;
-                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] + generalPurposeRegisters[(opCode & 0x00F0) >> 4];
+                case 0x0004:
                 break;
-                
-                case 0x0005: //8xy5 - SUB Vx, Vy
-                    registerVF = generalPurposeRegisters[(opCode & 0x0F00) >> 8] > generalPurposeRegisters[(opCode & 0x00F0) >> 4];
-                    generalPurposeRegisters[(opCode & 0xF000) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] - generalPurposeRegisters[(opCode & 0x00F0) >> 4];
+                case 0x0006:
                 break;
-                
-                case 0x0006: //8xy6 - SHR Vx {, Vy}
-                    registerVF = generalPurposeRegisters[(opCode & 0x0F00) >> 8] & 0x01 == 0x01;
-                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] / 2;
+                case 0x0007:
                 break;
-                
-                case 0x0007: //8xy7 - SUBN Vx, Vy
-                    registerVF = generalPurposeRegisters[(opCode & 0x00F0) >> 4] > generalPurposeRegisters[(opCode & 0x0F00) >> 8];
-                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x00F0) >> 4] - generalPurposeRegisters[(opCode & 0x0F00) >> 8];
+                case 0x000E:
                 break;
-                
-                case 0x000E: //8xyE - SHL Vx {, Vy}
-                    registerVF = generalPurposeRegisters[(opCode & 0x0F00) >> 8] & 0x80 == 0x80;
-                    generalPurposeRegisters[(opCode & 0x0F00) >> 8] = generalPurposeRegisters[(opCode & 0x0F00) >> 8] * 2;
-                break;
-                
                 default:
                 break;
+
+
             }
         break;
         case 0xA000:
@@ -77,7 +58,31 @@ void Cpu::processNextInstruction(){
         break;
         case 0xB000:
             programCounter = (opCode & 0x0FFF) + generalPurposeRegisters[0];
-        break;
+            break;
+        case 0xF000:
+            switch(opCode & 0x00FF){
+                case 0xF007:
+                    generalPurposeRegisters[opCode & 0x0F00 >> 8] = delayTimer;
+                    break;
+                case 0x000A:
+                case 0x0015:
+                    delayTimer = generalPurposeRegisters[opCode & 0x0F00 >> 8];
+                    break;
+                case 0x0018:
+                    soundTimer = generalPurposeRegisters[opCode & 0x0F00 >> 8];
+                    break;
+                case 0x001E:
+                    registerIndex += generalPurposeRegisters[opCode & 0x0F00 >> 8];
+                    break;
+                case 0x0029:
+                    registerIndex =  
+                case 0x0033:
+                     
+                    
+
+                case 0x0055:
+            }
+
         
     }
     programCounter += 2;//programCounter stores location of the currently executing op code so it should be updated after it has been processed not when loaded
