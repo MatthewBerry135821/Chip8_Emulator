570eafa96b14261d4242e0c6482744b53ddcaad8
MatthewBerry135821 <mb135821@ohio.edu>
2023-02-03 20:17:53 -0500

    Added Screen functionality

--- screen.h
+++ screen.h
@@ -2,8 +2,14 @@
 
 #ifndef SCREEN_H
 #define SCREEN_H
+
+#define SCREEN_SCALE 8
+#define SCREEN_WIDTH 64*SCREEN_SCALE
+#define SCREEN_HEIGHT 32*SCREEN_SCALE
 class Screen{
     public:
+        Screen();
+        ~Screen();
         /**
          * @brief draws a one byte sprite to the screen at position x, y
          * 
@@ -12,6 +18,12 @@ class Screen{
          * @param y 
          */
         void drawByte(uint8_t sprite, int x, int y);
+        void drawPixel(int color, int x, int y);
+        void initiateScreen();
+        void updateScreen();
     private:
+        SDL_Window *window;
+        SDL_Surface *screenSurface;
+        uint32_t *pixels;
 };
 #endif
--- system.cc
+++ system.cc
@@ -1,7 +1,10 @@
 #include "system.h"
 #include <iostream>
 #include <cstdint>
+
 using namespace std;
+
+
 
 System::System(){
     Cpu c(this);
@@ -9,7 +12,15 @@ System::System(){
 }
 void System::testImplementation(){
     int testType;
+    
+    screen.drawByte(0xF0,0,0);//draws A
+    screen.drawByte(0x90,0,1);
+    screen.drawByte(0xF0,0,2);
+    screen.drawByte(0x90,0,3);
+    screen.drawByte(0x90,0,4);
+
     do{
+        screen.updateScreen();
         cout << "\nTest Options:\n\t1) Test opCode\n\t2) Dump CPU state\n\t-1) Exit" << "\nEnter option: ";
         cin >> testType;
         switch(testType){
@@ -27,5 +38,6 @@ void System::testImplementation(){
                 cpu.dumpState();
             break;
         }
+        screen.updateScreen();
     }while(testType != -1);
 }
